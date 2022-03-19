fbe67218f8faa64e025b3b65b8047a04799f88d3
Rajanayaki <rajanayaki4444@gmail.com>
2022-03-19 20:43:06 +0530

    First Version of Friends app

---
+++ app/javascript/application.js
@@ -0,0 +1,12 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+
+//= require jquery
+//= require jquery-ui
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
+
+import "@hotwired/turbo-rails"
+import "controllers"
